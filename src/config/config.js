cdeb38afde49136658733718ea185d042748d83e
wachirapongng-crypto <wachirapong.ng@rmuti.ac.th>
2026-01-08 20:16:25 +0700

    update

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 export const CONFIG = {
   SHEET_ID: "1bkpz-iG4B8qnvZc4ql4qE15Qw8HrIZ1aeX1vZQzMFy0",
   // URL จากไฟล์ dashboard.js เดิม
-  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyfI-AhBo8fBlKlXoPqg89StSNFqo89TFbMoXEhLHUzyfk6m92DdIdo0k77rRfKBx85FQ/exec",
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyZAYiuODSmH7DalciLFxZayptUH9oIkf1TwzCUo0D8-Cf3X36LAuhbID4gEgZBN7kdXQ/exec",
 };
 
 export const SHEET_NAMES = {
