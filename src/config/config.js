7fba265ae33b70459975019e70a03b8a07e05e7c
wachirapongng-crypto <wachirapong.ng@rmuti.ac.th>
2026-01-08 20:55:01 +0700

    update

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 export const CONFIG = {
   SHEET_ID: "1bkpz-iG4B8qnvZc4ql4qE15Qw8HrIZ1aeX1vZQzMFy0",
   // URL จากไฟล์ dashboard.js เดิม
-  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyZAYiuODSmH7DalciLFxZayptUH9oIkf1TwzCUo0D8-Cf3X36LAuhbID4gEgZBN7kdXQ/exec",
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzzNsnmILlS61EvZ8zAcP8aDCWIK00WccCv8GsCmFO6HY4yoWFK7DSLVOWT7ll1aJCAvA/exec",
 };
 
 export const SHEET_NAMES = {
